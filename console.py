d1bde7f6672fa218880e993e29558c03c7a4b1d7
Jose Carlos Jiménez <CarlostartMail@gmail.com>
2022-04-21 21:44:30 +0200

    Mejora en el crop + Pequeño cambio sintaxis de comandos + Aceptar carpetas como input

--- console.py
+++ console.py
@@ -1,13 +1,25 @@
-from cv2 import readOpticalFlow
+import traceback
+import pandas as pd
 from imProcessing import ImProcessing
 import sys
+import os
+import csv
 
 file_paths = "path_to_data.txt"
+file_csv = "DataSet.csv"
+col_list = ['ID', 'OCR_1', 'OCR_2', 'OCR_3', 'HU_1', 'HU_2']
 
 def readPaths():
     try:
         with open(file_paths, 'r') as f:
-            coin_imgs = f.readlines()
+            lines = f.readlines()
+            coin_imgs = []
+            for p in lines:
+                if p[-2]== '/' or p[-2] == '\\':
+                    coin_imgs += map(p[:-1].__add__,os.listdir(p[:-1]))
+                else:
+                    coin_imgs.append(p[:-1])
+
             return coin_imgs
     except FileNotFoundError:
         return []
@@ -24,11 +36,11 @@ def setImg(args):
         if coin_imgs == []:
             print("Escribe img PATH para seleccionar la imagen de la moneda: "+
                     "find PATH_1 PATH_2 ... PATH_N\n" +
-                    "OPCIONES: --a (Añadir a la lista), --w (Escribir encima de la lista)\n"
-                    "Tambien puedes especificar si hay multiples monedas en la imagen:\n" +
-                    "find (M)PATH [...]")
+                    "OPCIONES: --a (Añadir a la lista), --w (Escribir encima de la lista)\n"+
+                    "Se pueden seleccionar carpetas")
+
         else:
-            print("Imagenes seleccionadas:\n" + ''.join(coin_imgs))
+            print("Imagenes seleccionadas: " + ', '.join(coin_imgs))
     else:
         if args[0][:2] == '--':
             if args[0][2] == 'a':
@@ -48,72 +60,112 @@ def findCoins(coin_imgs):
         if coin_imgs == []:
             print("Selecciona la o las imagenes a identificar:\n" +
                     "find PATH_1 PATH_2 ... PATH_N\n" +
-                    "Tambien puedes especificar si hay multiples monedas en la imagen:\n" +
-                    "find (M)PATH [...]")
+                    "Se pueden seleccionar carpetas")
             return
     # Identifica todas las imagenes especificadas
-    for img in coin_imgs:
+    for pth in coin_imgs:
         try:
-            # Si hay multiples monedas en la imagen debe estar indicado con (M)
-            if img[0:3] == '(M)':
-                findCoin(img[3:], True)
+            # Si hay multiples monedas en la imagen debe estar indicado con M
+            img = pth.split('/')[-1]
+            if img[0] == 'M':
+                nc = int(img.split('_')[0][1:])
+                findCoin(pth, nc)
             else:
-                findCoin(img, False)
+                findCoin(pth)
         except FileNotFoundError:  # Tratamos cuando no encuentra el archivo
             print("Archivo no encontrado")
 
-def findCoin(img, multiple):
-    print("Identificando imagen: " + img)
+def findCoin(img, ncoins = 1):
     # Extraemos datos de la moneda
-    data = ImProcessing.extractData(img, multiple)
-    print(data)
+    data = ImProcessing.extractData(img, ncoins)
+
+    for d in data:
+        ocr, hu, keyp = d[1], d[2], d[3]
+        print(f'OCR: {ocr}')
+        print(f'Hu: {hu}')
+        print(f'KeyP: {keyp}')
 
     # -- POR COMPLETAR --
     # Añadir identificacion por aprendizaje
 
-def addCoin(coin_imgs):
+
+
+def addCoins(args):
     # Si no se especifica ninguna dirección, identifica la anteriormente seleccionada
-    if coin_imgs == []:
+    if args == []:
         print("Añade información de la moneda:\n" +
                 "add PATH_1 PATH_2 ... PATH_N {Info}\n" +
-                "Tambien puedes especificar si hay multiples monedas en la imagen:\n" +
-                "add (M)PATH{Info}")
+                "Se pueden seleccionar carpetas")
         return
 
     # Separa imagenes de atributos
-    if len(coin_imgs) == 1:
-        imgs = readPaths()
-        info = coin_imgs[0]
-    else:
-        imgs = coin_imgs[0:-1]
-        info = coin_imgs[-1]
-    # Lee la información obtenida de cada moneda
-    # try:
-    # Obtenemos los atributos
-    info = info[1:-1].split(",")
-    info_dict = {}
-    # Los metemos en un diccionario
-    for atb in info:
-        key_value = atb.split(":")
-        info_dict.update({key_value[0]: key_value[1]})
-    print(info_dict)
-
-    # Extraemos datos de todas la imagenes especificadas
-    img_data = []
-    for img in imgs:
-        # Si se especifica que hay multiples monedas en la imagen
-        if img[0:3] == '(M)':
-            img_data.append(
-                ImProcessing.extractData(img[3:], True)[0])
+    try:
+        if len(args) == 1:
+            imgs = readPaths()
+            info = args[0]
         else:
-            img_data.append(ImProcessing.extractData(img, False))
+            imgs = args[0:-1]
+            info = args[-1]
+        # Lee la información obtenida de cada moneda
 
-    print(img_data)
+        # Obtenemos los atributos
+        info = info[1:-1].split(",")
+        info_dict = {}
+        # Los metemos en un diccionario
+        for atb in info:
+            key_value = atb.split(":")
+            info_dict.update({key_value[0]: key_value[1]})
+        print(info_dict)
 
-    # -- POR COMPLETAR --
-    # Añadir datos al DataSet del aprendizaje
-    # except:
-    print("Error: Seguir formato -> PATH_1,PATH_2,...PATH_N{Info}")
+        # Extraemos datos de todas la imagenes especificadas
+        to_csv = []
+        for pth in imgs:
+            # Si hay multiples monedas en la imagen debe estar indicado con M
+            img = pth.split('/')[-1]
+            if img[0] == 'M':
+                nc = int(img.split('_')[0][1:])
+                print(img)
+                id = int(img.split('_')[1][:])
+                to_csv += getData(pth, ncoins=nc, id=id)
+            else:
+                to_csv += getData(pth, id=int(img.split('_')[0]))
+            
+        writeCSV(to_csv)
+        # -- POR COMPLETAR --
+
+    except FileNotFoundError:  # Tratamos cuando no encuentra el archivo
+        print("Archivo no encontrado")
+    except:
+        traceback.print_exc()
+        print("Error: Seguir formato -> PATH_1,PATH_2,...PATH_N{Info}")
+
+def getData(img, id, ncoins = 1):
+    # Extraemos datos de la moneda
+    data = []
+    data += ImProcessing.extractData(img, ncoins)
+
+    to_csv = []
+    for d in data:
+        ocr, hu, keyp = d[1], d[2], d[3]
+        # print(f'OCR: {ocr}')
+        # print(f'Hu: {hu}')
+        # print(f'KeyP: {keyp}')
+        row = [str(id)]
+        row += ocr[:min(len(ocr),3)]
+        for i in range(len(row), 4):
+            row.append('')
+        row += str(hu[0]),str(hu[1])
+        to_csv.append(row)
+
+    return to_csv
+
+
+def writeCSV(to_csv):
+    # ['ID', 'OCR_1', 'OCR_2', 'OCR_3', 'HU_1', 'HU_2']
+    with open(file_csv, mode='w') as f:
+        csvwriter = csv.writer(f) 
+        csvwriter.writerow(col_list) 
+        csvwriter.writerows(to_csv)
 
 def invalidCommand(args):
     print("Comando no existe")
@@ -128,7 +180,7 @@ def processCommand():
     select_action = {
         "img": setImg,
         "find": findCoins,
-        "add": addCoin
+        "add": addCoins
     }
     select_action.get(command[0], invalidCommand)(command[1:])
 
--- imProcessing.py
+++ imProcessing.py
@@ -3,19 +3,27 @@ import numpy as np
 import cv2
 import easyocr
 import pandas
-import csv
 
 class ImProcessing:
 
     @classmethod
-    def extractData(self, img_path, multiple=False):
+    def extractData(self, img_path, ncoins=1):
+        print(f"Identificando imagen: {img_path}")
         img = cv2.imread(img_path)
-        # Ajustes de imagen para que sigan un mismo formato
-        img = self.increaseContrast(img)
-        images = self.cropCircle(img, multiple)
+        # -- Ajustes de imagen para que sigan un mismo formato --
+        img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)  # Grayscale
+        # Normalizamos
+        img = cv2.normalize(img, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX)
+        # self.increaseContrast(cv2.cvtColor(img,cv2.COLOR_GRAY2BGR))
+
+        images = self.cropCircle(img, ncoins)
 
         data = []
-        for image in images if multiple else images[:1]:
+        # Si hay varias monedas en una misma imagen procesamos todas
+        for image in images if ncoins > 1 else images[:1]:
+            
+            # copy = image.copy()
+            # copy = cv2.Canny(copy,50,100)
 
             ocr_data = self.getOCR(image)      # Lectura de caracteres
             Hu = self.huMoments(image)    # Obtenemos los momentos de Hu
@@ -25,6 +33,7 @@ class ImProcessing:
             # Espeamos a pulsar una tecla para cerrar las imágenes
             # cv2.waitKey(0)
             # cv2.destroyAllWindows()
+            plt.show()
             data.append((image, ocr_data, Hu, keyP))
 
         return data
@@ -44,70 +53,108 @@ class ImProcessing:
         img = cv2.cvtColor(cv2.cvtColor(
             limg, cv2.COLOR_LAB2BGR), cv2.COLOR_BGR2GRAY)
 
-        return img
 
     @staticmethod
-    def cropCircle(img, multiple=False):
+    def cropCircle(img, ncoins=1):
+        height, width = img.shape
         # -- Primero modificamos la imagen para facilitar la detección de circulos --
         #   Creamos una copia de la imagen para poderla modificar
         gray = img.copy()
         #   Aplicamos un umbral
+        gray = cv2.GaussianBlur(img, (19, 19), 5)
+        gray = gray*2
         _, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
-        
+       
+        edges = cv2.Canny(gray, 50, 100)
         # -- {DEBUG} --
         # cv2.imshow("thresh", thresh)
         # cv2.waitKey(0)
-        # plt.imshow(thresh,'gray')
-        # plt.show()
+        plt.subplot(241)
+        plt.imshow(edges,'gray')
+        plt.title('Canny')
+        #   Eliminamos artefactos y suavizamos superficie
 
         #   Aseguramos que el fondo sea negro
-        if thresh[0, 0] != 0:
+        if np.mean(thresh[0, :]) > 127:
             thresh = (255-thresh)
-
-        #   Usaremos un kernel 5x5
-        kernel = np.ones((5, 5), np.uint8)
-        #   Aplicamos 5 dilataciones
-        thresh = cv2.dilate(thresh, kernel, iterations=2)
-        
-        # -- {DEBUG} --
-        # cv2.imshow("1", thresh)
-        # cv2.waitKey(0)
+        plt.subplot(242)
+        plt.imshow(thresh, 'gray')
+        plt.title("Thresh")
         # plt.imshow(thresh,'gray')
         # plt.show()
+        #   Usaremos un kernel 5x5
+        kernel = np.ones((5, 5), np.uint8)
+         
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (19, 19))
 
-        #   Aplicamos una erosion
-        # thresh = cv2.erode(thresh, kernel)
+        morphed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel,iterations=1)
+        plt.subplot(243)
+        plt.imshow(morphed, 'gray')
+        plt.title("Morphed")
+        # morphed = cv2.morphologyEx(morphed, cv2.MORPH_OPEN, kernel,iterations=1)
+        # plt.subplot(223)
+        # plt.imshow(morphed, 'gray')
+        # plt.title("OPEN")
+        # morphed = cv2.morphologyEx(morphed, cv2.MORPH_DILATE,kernel,iterations=5)
+        # plt.subplot(224)
+        # plt.imshow(morphed, 'gray')
+        # plt.title("DILATE")
 
-        # cv2.imshow("2", thresh)
-        # cv2.waitKey(0)
+        contours = cv2.findContours(morphed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours = contours[0] if len(contours) == 2 else contours[1]
+        big_contour = max(contours, key=cv2.contourArea)
 
-        #   Eliminamos artefactos y suavizamos superficie
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))
+        for row in range(height):
+            if morphed[row, 0] == 255 and [0,row] not in big_contour:
+                cv2.floodFill(morphed, None, (0, row), 0)
+            if morphed[row, width-1] == 255 and [width-1,row] not in big_contour:
+                cv2.floodFill(morphed, None, (width-1, row), 0)
+        for col in range(width):
+            if morphed[0, col] == 255 and [col,0] not in big_contour:
+                cv2.floodFill(morphed, None, (col, 0), 0)
+            if morphed[height-1, col] == 255 and [col,height-1] not in big_contour:
+                cv2.floodFill(morphed, None, (col, height-1), 0)
 
-        thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-        thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+        cv2.drawContours(morphed, [big_contour], 0, 255)
 
         # -- {DEBUG} --
-        # cv2.imshow('3', thresh)
+        # cv2.imshow('3', morphed)
         # cv2.waitKey()
         # cv2.destroyAllWindows()
-        plt.subplot(221)
-        plt.imshow(thresh,'gray')
-
-
+        plt.subplot(244)
+        plt.imshow(morphed,'gray')
+        plt.title("No borders shapes")
+        # plt.show()
+        print("Con canny")
+        circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 4,
+                        255, param1=50, param2=100, minRadius=127, maxRadius=0)
         # Detección de circulos
-        circles = cv2.HoughCircles(thresh, cv2.HOUGH_GRADIENT, 4,
-                                   255, param1=50, param2=250, minRadius=127, maxRadius=0)
+        if circles is None:
+            print("NONE Morphed")
+            circles = cv2.HoughCircles(morphed, cv2.HOUGH_GRADIENT, 4,
+                                    255, param1=100, param2=250, minRadius=127, maxRadius=0)
         # Si no encuentra hace una busqueda con mayor indice de error
         if circles is None:
-            circles = cv2.HoughCircles(thresh, cv2.HOUGH_GRADIENT, 4,
+            print("NONE2  Aumenta param 2")
+            circles = cv2.HoughCircles(morphed, cv2.HOUGH_GRADIENT, 4,
                                        255, param1=50, param2=100, minRadius=127, maxRadius=0)
-        print(circles)
+        if circles is None:
+            print("NON3 Canny Morphed")
+            edges = cv2.Canny(morphed, 50, 100)
+            plt.subplot(244)
+            plt.imshow(edges,'gray')
+            plt.title('Canny en morphed')
+            circles = cv2.HoughCircles(edges, cv2.HOUGH_GRADIENT, 4,
+                                    255, param1=50, param2=100, minRadius=127, maxRadius=0)
+
+        print(f"Detectados {len(circles[0,:])} Circulos")
+        if ncoins > 1:
+            mediana = np.median(circles[0,:,2])
+            print(f"Mediana: {mediana}")
 
         imgs = []
-        height, width = img.shape
         gray = cv2.cvtColor(gray,cv2.COLOR_GRAY2BGR)
-        for i in circles[0, :]:
+        for idx, i in enumerate(circles[0, :]):
             # Eliminamos el anillo exterior de la moneda
             # -- Creamos una máscara --
             mask = np.zeros((height, width), np.uint8)
@@ -116,6 +163,11 @@ class ImProcessing:
             x, y, r = i
             x, y, r = int(x), int(y), int(r)
             r = min(x,y,width-x,height-y, r)
+
+            # Borra circulos de tamaño desproporcionado
+            if ncoins > 1 and (r > mediana*1.35 or r < mediana*0.35) and idx < len(circles[0,:]):
+                # print(f'Borra circulo: x={x} y={y} r={r}')
+                continue
 
             cv2.circle(mask, (x,y), r, (255, 255, 255),
                        thickness=-1)
@@ -135,10 +187,11 @@ class ImProcessing:
             # -- {DEBUG} --
             # cv2.imshow("final", cropped)
             # cv2.waitKey(0)
-            plt.subplot(222)
+            plt.subplot(245)
+            plt.title("Circles")
             plt.imshow(gray)
 
-            if not multiple:
+            if idx == ncoins - 1:
                 break
 
         # -- {DEBUG} --
@@ -146,7 +199,7 @@ class ImProcessing:
         # plt.imshow(gray)
         # plt.show()
         # cv2.destroyAllWindows()
-        return imgs
+        return imgs[:ncoins+1]
 
     @staticmethod
     def getOCR(img):
@@ -168,7 +221,7 @@ class ImProcessing:
             textos.append(tupla[1])
 
         # cv2.imshow("OCR_img", img)
-        plt.subplot(223)
+        plt.subplot(246)
         plt.imshow(copy)
         return textos
 
@@ -191,9 +244,8 @@ class ImProcessing:
             cv2.circle(kp, (int(x), int(y)), 3, (0, 0, 255), cv2.FILLED)
 
         # cv2.imshow("keypoints mineigenvalues", kp)
-        plt.subplot(224)
+        plt.subplot(247)
         plt.imshow(kp,'gray')
-        plt.show()
 
         return dst1
 
